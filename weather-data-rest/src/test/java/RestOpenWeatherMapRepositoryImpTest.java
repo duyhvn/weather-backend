db81c2988b7a5be82dabf2332d5ecbd0405d87af
pgtc97 <pgtc97@gmail.com>
2022-01-26 08:55:39 +0700

    update test

--- weather-data-rest/src/test/java/RestOpenWeatherMapRepositoryImpTest.java
+++
@@ -1,9 +0,0 @@
-import com.oddle.app.weather.service.WeatherService;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = WeatherService.class)
-public class RestOpenWeatherMapRepositoryImpTest {
-}
